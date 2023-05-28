ab19dac940992e25f5f23bfb35cfc387118eadb4
renatrysaev <renat.rysaev@gmail.com>
2023-05-29 02:46:41 +0500

    Change react-toastify to antd.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import { Router } from "./Router";
 
 export const App = () => {
-  return (
-    <>
-      <Router />
-      <ToastContainer />
-    </>
-  );
+  return <Router />;
 };
